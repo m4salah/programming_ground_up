fe982b41b67de537776c4d5b85643e5bff3ed95a
Mohamed A. Salah <me@msalah.net>
2025-05-31 16:41:47 +0300

    optmize the build in rust

--- chapter4/square-rs/src/main.rs
+++ chapter4/square-rs/src/main.rs
@@ -6,7 +6,7 @@ unsafe extern "C" {
 
 fn main() {
     for n in 1..1_000_000_000 {
-        assert_eq!(unsafe { square(n) }, u64::pow(n, 2));
+        assert!(unsafe { square(n) } == u64::pow(n, 2));
     }
     println!("Paased!");
 }
