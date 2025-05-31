f7881ee4d2be7fd4dc549b4fcfedaf7d7ab22c48
Mohamed A. Salah <me@msalah.net>
2025-05-31 15:31:47 +0300

    square assembly

---
+++ chapter4/square.s
@@ -0,0 +1,24 @@
+.section .data
+.section .text
+.global _start
+
+_start:
+push $5
+call square
+add $8, %rsp
+
+mov %rax, %rdi
+mov $60, %rax
+syscall
+
+square:
+push %rbp
+mov %rsp, %rbp
+mov 16(%rbp), %rax
+mul %rax
+
+# end
+mov %rsp, %rbp
+pop %rbp
+ret
+
