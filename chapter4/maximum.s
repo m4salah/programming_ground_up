3ed07274ae41c14cbc77c7f28e958a41c77e299a
Mohamed A. Salah <me@msalah.net>
2025-06-01 09:21:57 +0300

    maximum function

---
+++ chapter4/maximum.s
@@ -0,0 +1,46 @@
+.section .data
+data_items: #These are the data items
+.quad 3,67,34,222,45,75,54,34,44,33,22,11,66
+
+.section .text
+
+.global _start
+.global maximum
+
+_start:
+push $13
+push $data_items
+call maximum
+add $16, %rsp
+
+exit:
+# exit syscall
+# https://electronicsreference.com/assembly-language/linux_syscalls/exit/
+mov %rax, %rdi
+mov $60, %rax
+syscall
+
+
+.type maximum, @function
+maximum:
+push %rbp
+mov %rsp, %rbp
+mov 16(%rbp), %rbx # Pointer to the first element in the list
+mov 24(%rbp), %rcx # The length of the list
+mov $0, %rdi       # The index iterator
+mov (%rbx, %rdi, 8), %rax
+
+loop:
+cmp %rdi, %rcx
+je loop_end
+inc %rdi
+cmp %rax, (%rbx, %rdi, 8)
+jle loop
+mov (%rbx, %rdi, 8), %rax
+jmp loop
+
+loop_end:
+mov %rbp, %rsp
+pop %rbp
+ret
+
