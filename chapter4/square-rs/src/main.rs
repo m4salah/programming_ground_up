66975f54b3407cf3c12351c687245ac9b863aa52
Mohamed A. Salah <me@msalah.net>
2025-06-01 09:21:44 +0300

    start from 0

--- chapter4/square-rs/src/main.rs
+++ chapter4/square-rs/src/main.rs
@@ -5,7 +5,7 @@ unsafe extern "C" {
 }
 
 fn main() {
-    for n in 1..1_000_000_000 {
+    for n in 0..1_000_000_000 {
         assert!(unsafe { square(n) } == u64::pow(n, 2));
     }
     println!("Paased!");
